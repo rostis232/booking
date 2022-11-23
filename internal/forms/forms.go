44b3b255f2d41d05db8afa7358c47d2951c5b3f4
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-11-23 23:06:02 +0200

    Server-side form validation. part 2

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -11,6 +11,7 @@ type Form struct {
 	Errors errors
 }
 
+
 // New initializes a form struct
 func New(data url.Values) *Form {
 	return &Form{
@@ -19,9 +20,15 @@ func New(data url.Values) *Form {
 	}
 }
 
+//Valid returns true if there are no errors, otherwise false
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
+		f.Errors.Add(field, "This field cannot be blank")
 		return false
 	} else {
 		return true
